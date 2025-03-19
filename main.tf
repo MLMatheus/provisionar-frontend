6937db5c372294b8e7a0b84d24c70707ab0f402f
Matheus Lima <matheus2018.1997@gmail.com>
2025-03-19 08:42:42 -0300

    fix: Correção no atributo que quebrou

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "aws_route53_record" "frontend_alias" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.frontend_distribution
+    name                   = aws_cloudfront_distribution.frontend_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.frontend_distribution.hosted_zone_id
     evaluate_target_health = false
   }
